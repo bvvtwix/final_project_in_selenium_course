ab2206885a18c9873477da7d57aab4c6c0e1a8b4
bvvtwix <46612713+bvvtwix@users.noreply.github.com>
2021-01-14 09:10:18 +0500

    add login page test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -8,10 +9,15 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
-    # try:
+
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_page()
-    # except NoSuchElementException:
-    #     print(f'element not found')
+
+def test_should_be_login_form_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+
