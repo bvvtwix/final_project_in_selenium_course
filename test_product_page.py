e22ac9235c3bb1125fd5f6e9af343d04943f7b86
bvvtwix <46612713+bvvtwix@users.noreply.github.com>
2021-01-19 22:20:37 +0500

    code style for final review

--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ from .pages.login_page import LoginPage
 import pytest
 import time
 import random
+
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -13,7 +14,7 @@ import random
 #                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
@@ -50,13 +51,16 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+
 class TestUserAddToBasketFromProductPage():
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser, setup):
         link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
         page = ProductPage(browser, link)
